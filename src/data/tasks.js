fc066e6e7192495ce63ba3589f51b94365a726bc
aceDavon <makojidavid@gmail.com>
2022-08-09 17:59:36 +0100

    Extract tasks array to an seperate component
    Refactor code to allow for using a seperate component to create tasks
     array and import wher necessary

---
+++ src/data/tasks.js
@@ -0,0 +1,6 @@
+export const Data = [
+  { completed: false, description: 'Refactor code to ES6 modules', id: 1 },
+  { completed: false, description: 'Refactor code to ES6 modules', id: 1 },
+  { completed: false, description: 'Refactor code to ES6 modules', id: 1 },
+  { completed: false, description: 'Refactor code to ES6 modules', id: 1 },
+];
